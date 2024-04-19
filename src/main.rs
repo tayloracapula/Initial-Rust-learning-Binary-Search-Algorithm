bb256e19d3556fc7d1ff5b979fa40e98f5605a57
Fred Taylor <alfred.taylor@capula.com>
2024-04-19 11:52:40 +0100

    completed an exponential sort function

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn ingest() -> String {
 }
 
 fn binary_search(arr: &[Scientist], target: &str) -> Option<usize> {
-    let target = target.to_lowercase();
+    let target: String = target.to_lowercase();
     let mut min: usize = 0;
     let mut max: usize = arr.len() - 1;
     while max >= min {
@@ -39,8 +39,23 @@ fn binary_search(arr: &[Scientist], target: &str) -> Option<usize> {
     }
     return None;
 }
+fn exponential_search(_arr: &[Scientist], _target: &str, size: usize) -> Option<Option<usize>>{
+    if size == 0 {
+        return None;
+    } else if _arr[0].last_name == _target {
+        return Some(Some(0));
+    }
 
-fn main() {
+    let mut bound: usize = 1;
+
+    while bound < size && _arr[bound].last_name < _target {
+        bound *= 2;
+    }
+
+    return Some(binary_search(&_arr, _target));
+}
+
+fn scientist_names_def() -> [Scientist<'static>; 10] {
     let scientist_names: [Scientist; 10] = [
         Scientist {
             first_name: "Tim",
@@ -83,7 +98,12 @@ fn main() {
             last_name: "Torvalds",
         },
     ];
+    return scientist_names;
+}
+
+fn scientist_sorter(scientist_names: [Scientist<'_>; 10]) -> [Scientist<'_>; 10] {
     let mut scientists_vec: Vec<Scientist> = scientist_names.iter().cloned().collect();
+
     scientists_vec.sort_by(|a, b| a.last_name.cmp(&b.last_name));
     let sorted_scientists: [Scientist; 10] =
         scientists_vec
@@ -92,11 +112,31 @@ fn main() {
                 panic!("Expected a Vec of length {} but it was {}", 10, v.len())
             });
 
+    sorted_scientists
+}
+
+fn input_and_search_caller(sorted_scientists: &[Scientist<'_>; 10]) -> (String, Option<usize>) {
     print!("Please enter the name of a computer scientist: ");
     let input_fullname: String = ingest().parse().expect("could not parse input");
+
     let lowercase_name: String = input_fullname.to_lowercase();
+
     let last_name: &str = lowercase_name.split_whitespace().last().unwrap();
-    let found_index: Option<usize> = binary_search(&sorted_scientists, &last_name);
+
+    let sorted_scientists_length: usize = sorted_scientists.len();
+
+    let found_index: Option<usize> = exponential_search(sorted_scientists, &last_name, sorted_scientists_length).unwrap_or_else(|| None);
+
+    (input_fullname, found_index)
+}
+
+fn main() {
+    let scientist_names: [Scientist; 10] = scientist_names_def();
+
+    let sorted_scientists = scientist_sorter(scientist_names);
+
+    let (input_fullname, found_index) = input_and_search_caller(&sorted_scientists);
+
     match found_index {
         Some(index) => {
             println!(
